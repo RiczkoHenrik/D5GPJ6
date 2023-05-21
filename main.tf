be017ca3a7bf90200739bf6681087e7ee209b3e3
RiczkoHenrik <82958011+RiczkoHenrik@users.noreply.github.com>
2023-05-21 21:43:40 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,3 +20,9 @@ module "write" {
   answer_4 = trim(split("=", split("\n", local.answers)[3])[1], "\"\n")
   answer_5 = trim(split("=", split("\n", local.answers)[4])[1], "\"\n")
 }
+  
+module "data" {
+   source = "./modules/data"
+   file_path = module.files.example_output[0]
+   depends_on = [module.files]
+}  
