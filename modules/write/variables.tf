881159fc350eefd1752e1d68a5efa7a56c89ebae
RiczkoHenrik <82958011+RiczkoHenrik@users.noreply.github.com>
2023-05-21 20:52:19 +0200

    Update variables.tf

--- modules/write/variables.tf
+++ modules/write/variables.tf
@@ -1,1 +1,15 @@
-
+variable "answer_1" {
+  descriptin = "This is the first answer"
+}
+variable "answer_2" {
+  descriptin = "This is the second answer"
+}
+variable "answer_3" {
+  descriptin = "This is the third answer"
+}
+variable "answer_4" {
+  descriptin = "This is the fourth answer"
+}
+variable "answer_5" {
+  descriptin = "This is the fifth answer"
+}
